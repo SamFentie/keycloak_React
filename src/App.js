b89b1f00bac4cf030dc4eb5a441361da15d408ea
unknown <samuelfentie55@gmail.com>
2024-05-09 13:55:39 +0300

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { httpClient } from './HttpClient';
+import Keycloak from 'keycloak-js';
 
+/*
+  Init Options
+*/
+let initOptions = {
+  url: 'http://localhost:7070/',
+  realm: 'master',
+  clientId: 'react-client',
+}
+
+let kc = new Keycloak(initOptions);
+
+kc.init({
+  onLoad: 'login-required', // Supported values: 'check-sso' , 'login-required'
+  checkLoginIframe: true,
+  pkceMethod: 'S256'
+}).then((auth) => {
+  if (!auth) {
+    window.location.reload();
+  } else {
+    /* Remove below logs if you are using this on production */
+    console.info("Authenticated");
+    console.log('auth', auth)
+    console.log('Keycloak', kc)
+    console.log('Access Token', kc.token)
+
+    /* http client will use this header in every request it sends */
+    httpClient.defaults.headers.common['Authorization'] = `Bearer ${kc.token}`;
+
+    kc.onTokenExpired = () => {
+      console.log('token expired')
+    }
+  }
+}, () => {
+  /* Notify the user if necessary */
+  console.error("Authentication Failed");
+});
 function App() {
   return (
     <div className="App">
